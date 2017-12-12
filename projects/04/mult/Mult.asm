ec61018d15ba4ad0a41e1ab44d7e1ecb5f2870d2
Yuchen Pei <me@ypei.me>
2017-12-12 14:30:43 +0100

    finished project 4

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,27 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@i
+M=0
+@R2
+M=0
+
+(LOOP)
+@i
+M=M+1
+D=M
+@R0
+D=D-M
+@END
+D;JGT
+@R1
+D=M
+@R2
+M=M+D
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
