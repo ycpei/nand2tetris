19cf47dd1c6e9d7c265ed52270eebd9095b49a8c
Yuchen Pei <me@ypei.me>
2018-01-05 16:50:11 +0100

    checkpoint: refactored project08 code

--- projects/08/VMTranslator.hs
+++ projects/08/VMTranslator.hs
@@ -3,54 +3,58 @@ import Data.List.Split (splitOn)
 import System.Environment (getArgs)
 import Data.Maybe (fromJust)
 import Data.List (elemIndex)
+import System.Directory (listDirectory)
 
 --preamble = "@256\nD=A\n@SP\nM=D\n"
 preamble = ""
 
 epilogue = "(END)\n@END\n0;JMP"
 
-parse' :: [Char] -> [[Char]] -> Int -> [Char] -> [Char]
+-- parse' command restOfCommand vmLineNumber fileName functionName = asm code
+parse' :: [Char] -> [[Char]] -> Int -> [Char] -> [Char] -> [Char]
 
-parse' "return" [] _ _ = backupLCL ++ popToARG ++ moveSP ++ restore "THAT" 
-                         ++ restore "THIS" ++ restore "ARG" ++ restore "LCL"
-                         ++ gotoRet
+parse' "return" [] _ _ _ = backupLCL ++ popToARG ++ moveSP ++ restore "THAT" 
+                           ++ restore "THIS" ++ restore "ARG" ++ restore "LCL"
+                           ++ gotoRet
   where backupLCL = "@LCL\nD=M\n@R13\nM=D\n"
         popToARG = pop "M" ++ "@ARG\nA=M\nM=D\n"
         moveSP = "@ARG\nD=M+1\n@SP\nM=D\n"
         restore name = "@R13\nAM=M-1\nD=M\n@" ++ name ++ "\nM=D\n"
         gotoRet = "@R13\nA=M-1\nA=M\n0;JMP\n"
 
-parse' op [] n _
+parse' op [] n _ _
   | op `elem` ["add", "sub", "and", "or"] = pop "M" ++ pop ('M':(f op):"D") ++ push "D"
   | op `elem` ["eq", "gt", "lt"] = pop "M" ++ pop "M-D" ++ ifThenElse op n
   | otherwise = pop "M" ++ push (f op:"D")
     where f "add" = '+'; f "sub" = '-'; f "and" = '&'; 
           f "or" = '|'; f "neg" = '-'; f "not" = '!'
 
-parse' "push" ["constant", x] _ _ = "@" ++ x ++ "\nD=A\n" ++ push "D"
+parse' "push" ["constant", x] _ _ _ = "@" ++ x ++ "\nD=A\n" ++ push "D"
 
-parse' cmd [seg, x] _ _ | seg `elem` ["local", "argument", "this", "that"] =
+parse' cmd [seg, x] _ _ _ | seg `elem` ["local", "argument", "this", "that"] =
   case cmd of 
     "push" -> getAddr seg x ++ "A=D\nD=M\n" ++ push "D"
     "pop" -> getAddr seg x ++ "@R13\nM=D\n" ++ pop "M" ++ "@R13\nA=M\nM=D\n"
 
-parse' cmd [seg, x] _ _ | seg `elem` ["pointer", "temp"] = 
+parse' cmd [seg, x] _ _ _ | seg `elem` ["pointer", "temp"] = 
   case cmd of 
     "push" -> getAddr' seg x ++ "D=M\n" ++ push "D"
     "pop" -> pop "M" ++ getAddr' seg x ++ "M=D\n"
 
-parse' cmd ["static", x] _ filename =
+parse' cmd ["static", x] _ filename _ =
   case cmd of
     "push" -> getAddr'' x filename ++ "D=M\n" ++ push "D"
     "pop" -> pop "M" ++ getAddr'' x filename ++ "M=D\n"
 
-parse' "label" [x] _ _ = "(" ++ x ++ ")\n"
+parse' "label" [x] _ _ fName = "(" ++ fName ++ "$" ++ x ++ ")\n"
 
-parse' "goto" [x] _ _ = "@" ++ x ++ "\n0;JMP\n" 
+parse' "goto" [x] _ _ fName = "@" ++ fName ++ "$" ++ x ++ "\n0;JMP\n" 
 
-parse' "if-goto" [x] _ _ = pop "M" ++ "@" ++ x ++ "\nD;JNE\n"
+parse' "if-goto" [x] _ _ fName = pop "M" ++ "@" ++ fName ++ "$" ++ x ++ "\nD;JNE\n"
 
-parse' "function" [f, n] _ _ = "(" ++ f ++ ")\n" ++ (mconcat $ replicate (read n) $ push "0")
+parse' "function" [f, n] _ _ _ = "(" ++ f ++ ")\n" ++ (mconcat $ replicate (read n) $ push "0")
+
+--parse' "call" [f, m] n _ = 
 
 getAddr seg x = "@" ++ seg2Lab seg ++ "\nD=M\n@" ++ x ++ "\nD=A+D\n"
 
@@ -76,10 +80,11 @@ ifThenElse cond n = "@" ++ cond' ++ show n ++ "\nD;J" ++ cond' ++ "\n" ++ push "
 pop :: [Char] -> [Char]
 pop xs = "@SP\nAM=M-1\nD=" ++ xs ++ "\n"
 
-parseline :: [[Char]] -> Int -> [Char] -> [Char] -> [Char]
-parseline [] _ acc _ = acc
-parseline (line:lines) n acc filename = parseline lines (n + 1) (acc ++ parse' cmd target n filename) filename
+parseline :: [[Char]] -> Int -> [Char] -> [Char] -> [Char] -> [Char]
+parseline [] _ acc _ _ = acc
+parseline (line:lines) n acc filename funName = parseline lines (n + 1) (acc ++ parse' cmd target n filename funName') filename funName'
   where cmd:target = words line
+        funName' = if cmd == "function" then head target else funName
 
 stripJunk :: [Char] -> [[Char]]
 stripJunk = filter (not . isEmptyLine) . fmap (head . splitOn "//") . lines . replCrWithNl
@@ -88,7 +93,7 @@ isEmptyLine :: [Char] -> Bool
 isEmptyLine = null . filter (not . flip elem " \t") 
 
 parseCode :: [Char] -> [Char] -> [Char]
-parseCode xs filename = preamble ++ (parseline (stripJunk xs) 0 "" filename) ++ epilogue
+parseCode xs filename = preamble ++ (parseline (stripJunk xs) 0 "" filename "") ++ epilogue
 
 replCrWithNl :: [Char] -> [Char]
 replCrWithNl = fmap cr2nl 
@@ -98,10 +103,15 @@ replCrWithNl = fmap cr2nl
 lastSplit c xs = (take (prefix - 1) xs, drop prefix xs)
   where prefix = length xs - (fromJust . elemIndex c . reverse) xs
 
+
+-- assuming the input is a dir with a trailing '/'
 main = do
-  args <- getArgs
-  let path = head args
-  let pathWithoutExt = fst $ lastSplit '.' path
-  let filename = snd $ lastSplit '/' pathWithoutExt
-  code <- readFile path
-  writeFile (pathWithoutExt ++ ".asm") (parseCode code filename)
+  dir <- head <$> getArgs
+  filesWODir <- filter isVMfile <$> listDirectory dir 
+  let vmFiles = (dir++) <$> filesWODir
+  let ofPath = dir ++ (snd $ lastSplit '/' $ init dir) ++ ".asm"
+  let filenames = removeExt <$> filesWODir
+  codes <- sequence $ readFile <$> vmFiles
+  writeFile ofPath (mconcat $ zipWith parseCode codes filenames)
+    where isVMfile xs = drop (length xs - 3) xs == ".vm"
+          removeExt xs = take (length xs - 3) xs
